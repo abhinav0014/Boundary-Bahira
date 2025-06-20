cc1a22a6d5b98a6696dcb1e0dd3fd3fcf3a010f5
Abhinav Gyawali <104615184+abhinav0014@users.noreply.github.com>
2025-06-20 18:24:34 +0545

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StaticViewSitemap
 
 sitemaps = {
@@ -11,5 +12,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('news/', news_list, name='news'),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
